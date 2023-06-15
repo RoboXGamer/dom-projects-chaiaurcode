d2a2123b25ab51c6b73a38f9bb4f597a709426a7
RoboXGamer <110761265+RoboXGamer@users.noreply.github.com>
2023-06-15 18:48:35 +0530

    3 files modified

--- 1-colorChanger/chaiaurcode.js
+++ 1-colorChanger/chaiaurcode.js
@@ -2,7 +2,9 @@ const buttons = document.querySelectorAll('.button');
 const body = document.body;
 
 buttons.forEach((button) =>
-  button.addEventListener('click', () => {
-    body.style.backgroundColor = button.id;
+  button.addEventListener('click', (e) => {
+    // body.style.backgroundColor = button.id; //This also works but below one is better
+
+    body.style.backgroundColor = e.target.id;
   })
 );
